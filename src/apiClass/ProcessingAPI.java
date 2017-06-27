2cde1fe4b2f6b8b04fba50ffb5e97da6e8de867c
Abira Roychoudhury <abira_roychoudhury@persistent.co.in>
2017-06-27 19:04:24 +0530

    first commit

---
+++ src/apiClass/ProcessingAPI.java
@@ -0,0 +1,5 @@
+package apiClass;
+
+public class ProcessingAPI {
+
+}
---
+++ src/templates/PanCard.java
@@ -0,0 +1,44 @@
+package templates;
+
+import java.util.Date;
+
+public class PanCard {
+	
+	private String firstName;
+	private String lastName;
+	private String fatherName;
+	private Date dob;
+	private String panNumber;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFatherName() {
+		return fatherName;
+	}
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getPanNumber() {
+		return panNumber;
+	}
+	public void setPanNumber(String panNumber) {
+		this.panNumber = panNumber;
+	}
+	
+}
