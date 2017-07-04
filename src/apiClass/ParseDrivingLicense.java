468f109cc1062d16c3e9a4210a1e1f4032914293
akshay_mhapankar <akshay_mhapankar@persistent.co.in>
2017-07-04 10:28:13 +0530

    Minor change

--- src/apiClass/ParseDrivingLicense.java
+++ src/apiClass/ParseDrivingLicense.java
@@ -48,7 +48,7 @@ public class ParseDrivingLicense {
 			else if(token.toLowerCase().contains("name")){
 				
 				String name = token.substring(token.toLowerCase().lastIndexOf("name")+4);
-				obj.setName(name.toUpperCase().trim());
+				obj.setName(name.toUpperCase().replace(":", "").trim());
 				
 				//Find MiddleName
 				String middleName = tokens[i+1].substring(tokens[i+1].toLowerCase().lastIndexOf("of")+2);
@@ -60,7 +60,10 @@ public class ParseDrivingLicense {
 			else if(token.toLowerCase().contains("add")){
 				
 				String address = token.substring(token.toLowerCase().lastIndexOf("add")+3);
-				address = address.concat(tokens[i+1]);
+				
+				if(!tokens[i+1].toLowerCase().contains("pin"))
+					address = address.concat(tokens[i+1]);
+				
 				obj.setAddress(address.replace(":", ""));
 			
 				i++;
@@ -80,7 +83,8 @@ public class ParseDrivingLicense {
 	public static void main(String args[]) {
 		ParseDrivingLicense pdl = new ParseDrivingLicense();
 		
-		DrivingLicense dl = pdl.parseDrivingLicense("THE UNION OF INDIA\nJud MAHARASHTRA STATE MOTOR DRIVING LICENCE VAN\nDL No MH03 20080006266 DOI: 18-01-2008\nTwente wead Valid Till : 17-01-2028 (NT) AED 04-02-2011 FORM 7\nAUTHORISATION TO DRIVE FOLLOWING CLASS RULE 16 (2)\nOF VEHICLES THROUGHOUT INDIA\nCOV DO/\nMCWG 20-01-2011\nLMV 18-01-2008\nDOB : 06-09-1984 BG : AB+\nName TRUPTI KAMBLE\nS/DMW ofMADHUKAR KAMBLE\nAdd 1093151, KANNAMWAR NAGAR NO-2\nVIKHROLI (E) MUMBAI\nPIN :400083\nSignature &amp; ID of\nTssuing Authority: MH03 2011369\nSignature/Thumb\nImpression of Holder\n");
+		//DrivingLicense dl = pdl.parseDrivingLicense("THE UNION OF INDIA\nJud MAHARASHTRA STATE MOTOR DRIVING LICENCE VAN\nDL No MH03 20080006266 DOI: 18-01-2008\nTwente wead Valid Till : 17-01-2028 (NT) AED 04-02-2011 FORM 7\nAUTHORISATION TO DRIVE FOLLOWING CLASS RULE 16 (2)\nOF VEHICLES THROUGHOUT INDIA\nCOV DO/\nMCWG 20-01-2011\nLMV 18-01-2008\nDOB : 06-09-1984 BG : AB+\nName TRUPTI KAMBLE\nS/DMW ofMADHUKAR KAMBLE\nAdd 1093151, KANNAMWAR NAGAR NO-2\nVIKHROLI (E) MUMBAI\nPIN :400083\nSignature &amp; ID of\nTssuing Authority: MH03 2011369\nSignature/Thumb\nImpression of Holder\n");
+		DrivingLicense dl = pdl.parseDrivingLicense("THE UNION OF INDIA\nMAHARASHTRA STATE MOTOR DRIVING LICENCE\nDL No MH-1220050000188 ID 12-12-2005\nA Valid Till: 11-12-2025 (Non Trans)\n(Inv Crg) FORM 7\nRULE 16 (2)\nAUTHORISATION TO DRIVE FOLLOWING CLASS.\nOF VEHICLES THROUGHOUT INDIA\nCOV ID\nMCWG 12-12-2005\nBadge1:\nDOB 12-05-1986 BG : Not\nName POOJA M PALANDE\nS/DMW of MILIND PALANDE\nAdd S No. 3214A/3, AMBEGAON BK,\nBHARTI VIDYAPEETH BACKSIDE\nPUNE\nPIN 411046 a\nSignature &amp; ID of Signature Thumb\nissuing Authority MH-1220051 Impression of Holder\n");
 		System.out.println(dl);
 	}
 }
